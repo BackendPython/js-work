bd01301be4d19289095fbcffd849ca2e7a0a4524
Sodiqov <96613827+Sodiqovj@users.noreply.github.com>
2022-07-26 17:40:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const character = document.querySelector('.character')
+const fullBox = document.querySelector('.full')
+const idle = {
+    "1":'/images/idle/1.png',
+    "2":'/images/idle/2.png',
+    "3":'/images/idle/3.png',
+}
+const fire = {
+    "1":'/images/fire/1.png',
+    "2":'/images/fire/2.png',
+    "3":'/images/fire/3.png',
+}
+const walk = {
+    "1":'/images/walk/1.png',
+    "2":'/images/walk/2.png',
+    "3":'/images/walk/3.png',
+    "4":'/images/walk/4.png',
+    "5":'/images/walk/5.png',
+    "6":'/images/walk/6.png',
+}
