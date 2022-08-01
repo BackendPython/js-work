541a3bf41a88599ce767263146d7357c22e62d80
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-01 19:14:06 +0500

    main

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const character = document.querySelector('.character')
+let zaxiraText = document.querySelector('.text')
 const fullBox = document.querySelector('.full')
 let characterStatus = 'idle'
 let statusTurn = false
 let backTime = 200
+
+let bodyWidth = parseInt(window.getComputedStyle(character).getPropertyValue('width'))
+let characterPostion = {
+    'position': 'left',
+    'check_left': bodyWidth / 10,
+}
 const idle = {
     "1":'/images/idle/1.png',
     "2":'/images/idle/2.png',
@@ -32,26 +39,67 @@ function idleTime(){
     }, 200);
     setTimeout(() => {
         character.style.backgroundImage = `url(${idle[3]})`
-        character.style.backgroundPosition = 'bottom'
     }, 400);
-    setTimeout(() => {z
+    setTimeout(() => {
         character.style.backgroundImage = `url(${idle[4]})`
     }, 600);
     setTimeout(() => {
         statusTurn = false
+        characterStatus = 'idle'
     }, 800);
 }
 
 function walkTime() {
-    character.style.transform = 'rotateY(180deg)'
+    character.style.backgroundImage = `url(${walk[1]})`
+    setTimeout(() => {
+        character.style.backgroundImage = `url(${walk[2]})`
+    }, 200);
+    setTimeout(() => {
+        character.style.backgroundImage = `url(${walk[3]})`
+    }, 400);
+    setTimeout(() => {
+        character.style.backgroundImage = `url(${walk[4]})`
+    }, 600);
+    setTimeout(() => {
+        character.style.backgroundImage = `url(${walk[5]})`
+    }, 800);
+    setTimeout(() => {
+        statusTurn = false
+        characterStatus = 'idle'
+        character.style.backgroundImage = `url(${walk[6]})`
+    }, 1000);
 }
 
+
 let characterTime = setInterval(function () {
+    let character_left = parseInt(window.getComputedStyle(character).getPropertyValue('left'))
+    let character_right = parseInt(window.getComputedStyle(character).getPropertyValue('right'))
+    character.style.left = `${characterPostion.check_left}px`
+    // checking idle
     if (characterStatus=='idle'&&statusTurn==false) {
+        statusTurn = true
         return idleTime()
     }
     if (characterStatus=='walk'&&statusTurn==false) {
+        statusTurn = true
         return walkTime()
     }
-    
+    zaxiraText.textContent = `left: ${character_left}    right: ${character_right}    body: ${bodyWidth}`
+}, 10)
+
+window.addEventListener('keyup', function(event){
+    switch (event.key) {
+        case 'ArrowLeft':
+            characterStatus = 'walk'
+            character.style.transform = 'rotateY(180deg)'
+            break;
+        case 'ArrowRight':
+            characterStatus = 'walk'
+            character.style.transform = 'rotateY(0deg)'
+            break;
+        // case 'ArrowTop':
+        //     characterStatus
+        default:
+            break;
+    }
 })
