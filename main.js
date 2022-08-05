85a211aac4112faa4a1c1f0ba8c83001442976a1
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-05 16:03:16 +0500

    main

--- main.js
+++ main.js
@@ -31,95 +31,27 @@ let check = setInterval(() => {
 });
 
 window.addEventListener('keyup', function(event){
+    let characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left'))
+    let characterRight = parseInt(window.getComputedStyle(character).getPropertyValue('right'))
     switch (event.key) {
         case "ArrowLeft":
             characterStatus = 'walk'
+            if (characterLeft>100) {
+                character.style.left = characterLeft - 100 + 'px'
+            }
             character.style.transform = 'rotateY(180deg)'
             break;
         case "ArrowRight":
             characterStatus = 'walk'
+            if (characterRight>100) {
+                character.style.left = characterLeft + 100 + 'px'
+            }
             character.style.transform = 'rotateY(0deg)'
             break;
-        case "":
+        case " ":
             characterStatus = 'attack'
-            character.style.transform = 'rotateY(0deg)'
             break;
         default:
             break;
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-// const idle = {
-//     "1":'/images/idle/idle-1.png',
-//     "2":'/images/idle/idle-2.png',
-//     // "3":'/images/idle/idle-3.png',
-//     "4":'/images/idle/idle-4.png',
-// }
-// const fire = {
-//     "1":'/images/fire/attck-1.png',
-//     "2":'/images/fire/attck-2.png',
-//     "3":'/images/fire/attck-3.png',
-// }
-// const walk = {
-//     "1":'/images/walk/walk-1.png',
-//     "2":'/images/walk/walk-2.png',
-//     "3":'/images/walk/walk-3.png',
-//     "4":'/images/walk/walk-4.png',
-//     "5":'/images/walk/walk-5.png',
-//     "6":'/images/walk/walk-6.png',
-// }
-
-// // character functions
-// function idleTime(){
-//     character.style.animation = 'idle 1s infinite'
-//     setTimeout(() => {
-//         statusTurn = false
-//         character.style.animation = 'idle 1s infinite'
-//     }, 1000);
-// }
-
-// function walkTime(){
-//     character.style.animation = 'walk 1s infinite'
-//     setTimeout(() => {
-//         statusTurn = false
-//         character.style.animation = 'idle 1s infinite'
-//     }, 1000);
-// }
-// function attackTime(){
-//     character.style.animation = 'attack 1s infinite'
-//     setTimeout(() => {
-//         statusTurn = false
-//         character.style.animation = 'idle 1s infinite'
-//     }, 1000);
-// }
-
-// let ckeckedStatus = setInterval(() => {
-//     if (characterStatus==='idle'&&statusTurn===false) {
-//         statusTurn = true
-//         idleTime()
-//     }
-//     if (characterStatus==='walk'&&statusTurn===false) {
-//         statusTurn = true
-//         walkTime()
-//         setTimeout(() => {
-//             character.style.animation = 'idle 1s infinite'
-//         }, 1000);
-//     }
-//     if (characterStatus==='attack'&&statusTurn===false) {
-//         statusTurn = true
-//         attackTime()
-//         setTimeout(() => {
-//             character.style.animation = 'idle 1s infinite'
-//         }, 1000);
-//     }
-// }, 10);
