31b0f58621491b4e7ae66c0f081b864e35b0b6a6
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-10 17:02:21 +0500

    main

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-const character = document.querySelector('.character')
-let zaxiraText = document.querySelector('.text')
-const fullBox = document.querySelector('.full')
-let rival = document.querySelectorAll('.rival')
-let characterStatus = 'idle'
-let rivalStatus = 'idle2'
-let statusTurn = false
-let gameStart = false
-
-
-
 // characterStatus checking
 let check = setInterval(() => {
     if (characterStatus=='idle') {
